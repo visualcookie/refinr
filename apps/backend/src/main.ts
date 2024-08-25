9d5cbc42604c6d77aafdd45a535f709e744f45bf
Dean Hidri <dean.hidri@swk.de>
2024-08-25 13:18:51 +0200

    fix: Enable cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://refinr.sloth.sh',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: '*',
+  });
   await app.listen(3000);
 }
 bootstrap();
