d014316bc091f04e8e0d1215f57d09272d6cab5d
Dean Hidri <dean.hidri@swk.de>
2024-08-30 09:54:19 +0200

    feat: Add analytics and speed insights for use in Vercel

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/providers/ThemeProvider";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +30,8 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
